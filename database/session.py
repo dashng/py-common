424fb3badf00838db74dc70bc84ad2e10d0f1fc9
shdai <shdai@cisco.com>
2018-02-23 08:54:17 +0800

    db session with class

--- database/session.py
+++ database/session.py
@@ -2,16 +2,33 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from contextlib import contextmanager
 
+class DB_Util(object):
+    DB_URL = 'sqlite:///./sqlalchemy.db'
+    def __init__(self):
+        pass
+
+    def session(self):
+        engine = self.engine()
+        _session = sessionmaker(bind=engine)
+        session = _session()
+        return session
+
+    def engine(self):
+        engine = create_engine(self.DB_URL)
+        return engine
+
 @contextmanager
 def session_scope():
-    engine = create_engine('sqlite:///./sqlalchemy.db', pool_size=20, max_overflow=0)
-    _session = sessionmaker(bind=engine)
-    session = _session()
     try:
-        yield session
+        db_util = DB_Util()
+        yield db_util.session()
         session.commit()
     except:
         session.rollback()
         raise
     finally:
         session.close()
+
+if __name__ == '__main__':
+    with session_scope() as session:
+       print (session)
